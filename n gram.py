30714f608a6645c35db85988729bced2e37dd4d7
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-09-09 14:57:37 +0530

    Calculating the n gram using adjacency list type thing. Uses lesser memory. Also did the trigram thing.

--- n gram.py
+++ n gram.py
@@ -38,13 +38,13 @@ def unigram(tokens):
         values = np.append(values, key)
         p = np.append(p, d[key] / float(total))
 
-    #generating sentences
+    # generating sentences
     c = 'a'
-    print("Unigram 1: ", end = " ")
+    print("Unigram 1: ", end=" ")
     while c not in end:
         c = np.random.choice(values, p=p)
         print(c, end=" ")
-    print("\nUnigram 2: ", end = " ")
+    print("\nUnigram 2: ", end=" ")
     c = 'a'
     while c not in end:
         c = np.random.choice(values, p=p)
@@ -74,22 +74,21 @@ def bigram(token_list):
     else:
         probability[prev]['.'] = 1
 
-
     for key in probability:
         div = 0
         for subkey in probability[key]:
             div += probability[key][subkey]
 
         for subkey in probability[key]:
-            probability[key][subkey] /= div
+            probability[key][subkey] /= float(div)
             val[key].append(subkey)
             p[key].append(probability[key][subkey])
 
     prev = '.'
     flag = True
     while prev not in end or flag:
-        prev = np.random.choice(val[prev], p = p[prev])
-        print(prev, end = " ")
+        prev = np.random.choice(val[prev], p=p[prev])
+        print(prev, end=" ")
         flag = False
     print()
 
@@ -100,6 +99,63 @@ def bigram(token_list):
         print(prev, end=" ")
         flag = False
     print()
+
+
+def trigram(token_list):
+    prev1 = '.'
+    prev2 = '.'
+    probability = {}
+    for token in token_list:
+        probability[token] = {}
+
+    probability[prev1] = {}
+    probability[prev1][prev2] = {}
+    for token in token_list:
+        if token in probability[prev1][prev2]:
+            probability[prev1][prev2][token] += 1
+        else:
+            probability[prev1][prev2][token] = 1
+        prev1 = prev2
+        prev2 = token
+        if prev2 not in probability[prev1]:
+            probability[prev1][prev2] = {}
+
+    for first in probability:
+        for second in probability[first]:
+            div = 0
+            for third in probability[first][second]:
+                div += probability[first][second][third]
+
+            for third in probability[first][second]:
+                probability[first][second][third] /= float(div)
+
+    prev1 = '.'
+    prev2 = 'the'
+    print(prev2, end = " ")
+    while prev2 not in end:
+        val = []
+        p = []
+        for key in probability[prev1][prev2]:
+            val.append(key)
+            p.append(probability[prev1][prev2][key])
+        prev1 = prev2
+        prev2 = np.random.choice(val, p = p )
+        print(prev2, end = " ")
+    print()
+    prev1 = '.'
+    prev2 = 'the'
+    print(prev2, end = " ")
+    while prev2 not in end:
+        val = []
+        p = []
+        for key in probability[prev1][prev2]:
+            val.append(key)
+            p.append(probability[prev1][prev2][key])
+        prev1 = prev2
+        prev2 = np.random.choice(val, p=p)
+        print(prev2, end=" ")
+    print()
+
 
 def graphics():
     path = '/home/nishantsinha15/Documents/sem7/Natural Language Processing/Assignment 3/20_newsgroups/comp.graphics/*'
@@ -118,8 +174,8 @@ def graphics():
             if exc.errno != errno.EISDIR:  # Do not fail if a directory is found, just ignore it.
                 raise  # Propagate other kinds of IOError.
     tokens = np.asarray(nltk.word_tokenize(article))
-    unigram(tokens)
-    bigram(tokens)
+    # unigram(tokens)
+    trigram(tokens)
 
 
 graphics()
